31e76bc713d59f83905d330a6b87f0a730f49137
Adrian B <adryanb1980@yahoo.com>
2024-12-05 16:22:56 +0200

    Browser updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,28 +25,52 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    
 
+
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://demoqa.com/',
+
+    headless:true,
+
+    //actionTimeout: 0,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
 
+    launchOptions: {
+      // 1
+      args: ["--start-maximized"],
+    },
+  },
   /* Configure projects for major browsers */
   projects: [
+  
+
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+
+      use: { 
+        
+        //...devices['Desktop Chrome'] 
+        viewport: null,
+      
+      },
+      
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { 
+        //...devices['Desktop Firefox'] 
+        viewport: null,
+      },
+      
     },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+      
     },
 
     /* Test against mobile viewports. */
@@ -60,14 +84,26 @@ export default defineConfig({
     // },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     {
+       name: 'Microsoft Edge',
+       use: { 
+       //...devices['Desktop Edge'],
+       viewport: null, 
+       channel: 'msedge' },
+      
+     },
+     {
+       name: 'Google Chrome',
+       use: {
+       
+        //...devices['Desktop Chrome'], 
+       viewport: null,
+       channel: 'chrome' },
+     
+     },
+
+
+
   ],
 
   /* Run your local dev server before starting the tests */
