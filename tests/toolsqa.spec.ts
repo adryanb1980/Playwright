fc4044005d46aa7c683ea8e30980d3118ddf799e
Adrian B <adryanb1980@yahoo.com>
2024-12-05 16:36:34 +0200

    Add report

--- tests/toolsqa.spec.ts
+++ tests/toolsqa.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Learning', async ({ page }, testInfo) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("DEMOQA");
+  //Login page screenshot
+  const screenshotloginpage = await page.screenshot();
+  await testInfo.attach('Login page', { body: screenshotloginpage, contentType: 'image/png' });
 });
