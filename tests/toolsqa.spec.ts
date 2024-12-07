5ed56f115bc643353b9208b6176c231d33e7f951
Adrian B <adryanb1980@yahoo.com>
2024-12-07 12:49:23 +0200

    Latest changes 7 Dec

--- tests/toolsqa.spec.ts
+++ tests/toolsqa.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import testdata from '../test-data/input.json';
 
-test('Elements - Text Box', async ({ page }, testInfo) => {
+test('Elements - Text Box', async ({ page }, testElements) => {
   test.setTimeout(120000);
   await page.goto('/');
 
@@ -9,26 +9,37 @@ test('Elements - Text Box', async ({ page }, testInfo) => {
   await expect(page).toHaveTitle("DEMOQA");
   //Login page screenshot
   const screenshotloginpage = await page.screenshot();
-  await testInfo.attach('DEMOQA page', { body: screenshotloginpage, contentType: 'image/png' });
+  await testElements.attach('DEMOQA page', { body: screenshotloginpage, contentType: 'image/png' });
 
   await page.locator('text=Elements').click();
   await page.mouse.up();
   await page.waitForTimeout(1000);
   const screenshotelements = await page.screenshot();
-  await testInfo.attach('Elements menu', { body: screenshotelements, contentType: 'image/png' });
+  await testElements.attach('Elements menu', { body: screenshotelements, contentType: 'image/png' });
 
-
-  
   await page.locator('text=Text Box').click();
   await page.getByPlaceholder('Full Name').fill(testdata.fullname);
-  
+  //Get elements by id
   await page.locator('#userEmail').fill(testdata.email);
   await page.locator('#currentAddress').fill(testdata.currentaddress);
   await page.locator('#permanentAddress').fill(testdata.permanentaddress);
 
   const textboxelementsfullname = await page.screenshot();
-  await testInfo.attach('Textbox area', { body: textboxelementsfullname, contentType: 'image/png' });
+  await testElements.attach('Textbox area', { body: textboxelementsfullname, contentType: 'image/png' });
 
+  //Click Submit
+  await page.locator('#submit').click();
+  await page.mouse.wheel(0, 300);
+  const submitbutton = await page.screenshot();
+  await testElements.attach('Submit', { body: submitbutton, contentType: 'image/png' });
+  await page.mouse.wheel(0, -300);
+
+
+
+  //Checkbox
+  await page.locator('text=Check Box').click();
+  const checkboxpoption = await page.screenshot();
+  await testElements.attach('CheckBox area', { body: checkboxpoption, contentType: 'image/png' });
   //Logout and close
   await page.waitForTimeout(4000);
   await page.close();
