e47545a4f41fc69bda55cc6fc73b0643da941cc4
Adrian B <adryanb1980@yahoo.com>
2024-12-10 18:15:55 +0200

    12.10-1

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,6 @@ export default defineConfig({
 
 
   use: {
-    
-
-
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://demoqa.com/',
 
--- tests/toolsqa.spec.ts
+++ tests/toolsqa.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 import textboxdata from '../test-data/textbox.json';
-import webtables from '../test-data/webtables.json';
-import * as Locators from '../locators/MenuOptionsLocators.json';
+import webtablesdata from '../test-data/webtables.json';
+import * as Locators from '../locators/Locators.json';
 
 test('Elements - Text Box', async ({ page }, testElements) => {
   test.setTimeout(120000);
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("DEMOQA");
-  //Login page screenshot
   const screenshotloginpage = await page.screenshot();
   await testElements.attach('DEMOQA page', { body: screenshotloginpage, contentType: 'image/png' });
-
   await page.locator(Locators.Elements.ElementsMenuOption).click();
   await page.mouse.up();
   const screenshotelements = await page.screenshot();
@@ -24,11 +20,8 @@ test('Elements - Text Box', async ({ page }, testElements) => {
   await page.locator(Locators.TextBox.UserEmail).fill(textboxdata.email);
   await page.locator(Locators.TextBox.CurrentAddress).fill(textboxdata.currentaddress);
   await page.locator(Locators.TextBox.PermanentAddress).fill(textboxdata.permanentaddress);
-
   const textboxelementsfullname = await page.screenshot();
   await testElements.attach('Textbox area', { body: textboxelementsfullname, contentType: 'image/png' });
-
-  //Click Submit
   await page.locator(Locators.TextBox.Submit).click();
   await page.mouse.wheel(0, 300);
   const submitbutton = await page.screenshot();
@@ -39,11 +32,9 @@ test('Elements - Text Box', async ({ page }, testElements) => {
   await page.locator(Locators.CheckBox.CheckBoxMenuOption).click();
   const checkboxoption = await page.screenshot();
   await testElements.attach('CheckBox area', { body: checkboxoption, contentType: 'image/png' });
-
   await page.locator(Locators.CheckBox.Home).click();
   const checkboxcheck = await page.screenshot();
   await testElements.attach('CheckBox checked', { body: checkboxcheck, contentType: 'image/png' });
-
   await page.getByRole('button', { name: 'Toggle' }).click();
   const checkboxexpand = await page.screenshot();
   await testElements.attach('CheckBox expanded', { body: checkboxexpand, contentType: 'image/png' });
@@ -59,20 +50,18 @@ test('Elements - Text Box', async ({ page }, testElements) => {
   //Web tables
   await page.locator(Locators.WebTables.WebTablesMenuOption).click();
   await page.locator(Locators.WebTables.AddNewRecordButton).click();
-  await page.locator(Locators.WebTables.FirstName).fill(webtables.webtablesfirstname);
-  await page.locator(Locators.WebTables.LastName).fill(webtables.webtableslastname);
-  await page.locator(Locators.WebTables.Email).fill(webtables.webtablesemail);
-  await page.locator(Locators.WebTables.Age).fill(webtables.webtablesage);
-  await page.locator(Locators.WebTables.Salary).fill(webtables.webtablessalary);
-  await page.locator(Locators.WebTables.Department).fill(webtables.webtablesdepartment);
-  await page.locator('#submit').click();
-  await page.locator('#searchBox').fill(webtables.webtablesfirstname);
-  //await page.waitForTimeout(1000);
+  await page.locator(Locators.WebTables.FirstName).fill(webtablesdata.webtablesfirstname);
+  await page.locator(Locators.WebTables.LastName).fill(webtablesdata.webtableslastname);
+  await page.locator(Locators.WebTables.Email).fill(webtablesdata.webtablesemail);
+  await page.locator(Locators.WebTables.Age).fill(webtablesdata.webtablesage);
+  await page.locator(Locators.WebTables.Salary).fill(webtablesdata.webtablessalary);
+  await page.locator(Locators.WebTables.Department).fill(webtablesdata.webtablesdepartment);
+  await page.locator(Locators.TextBox.Submit).click();
+  
+  await page.locator('#searchBox').fill(webtablesdata.webtablesfirstname);
   const webtablesoption1 = await page.screenshot();
   await testElements.attach('Web Tables area1', { body: webtablesoption1, contentType: 'image/png' });
   await page.locator('#searchBox').clear();
-  
-  //await page.waitForTimeout(1000);
   const webtablesoption = await page.screenshot();
   await testElements.attach('Web Tables area', { body: webtablesoption, contentType: 'image/png' });
 
