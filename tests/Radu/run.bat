a49b519c2f7ffdb5288d573f37a9ba76cb08c893
Adrian B <adryanb1980@yahoo.com>
2025-01-06 20:42:55 +0200

    Updates for filesystem operations

---
+++ tests/Radu/run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+@echo Welcome to bat file
+
+node test.js
--- tests/Radu/test.js
+++ tests/Radu/test.js
@@ -36,8 +36,7 @@ async function downloadfile(){
            console.log("Download Completed");
         });
      });
-    } 
-   
+    }   
 }
 
 
