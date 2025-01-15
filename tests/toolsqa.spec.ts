a92e5271e2a0e119ba4141b39a1e45cb0b571414
Adrian B <adryanb1980@yahoo.com>
2025-01-15 16:05:09 +0200

    Alerts - 15.01.2025

--- tests/toolsqa.spec.ts
+++ tests/toolsqa.spec.ts
@@ -26,7 +26,7 @@ test('DemoQA Test Report', async ({ page }, testReport) => {
   await page.locator(Locators.General.Submit).click();
   await page.mouse.wheel(0, 300);
   const submitbutton = await page.screenshot();
-  await testReport.attach('Submit', { body: submitbutton, contentType: 'image/png' });
+  await testReport.attach('Submit textboxes area', { body: submitbutton, contentType: 'image/png' });
   await page.mouse.wheel(0, -300);
 
 /*
@@ -98,7 +98,7 @@ test('DemoQA Test Report', async ({ page }, testReport) => {
   const nocontentAPI = await page.screenshot();
   await testReport.attach('NoContentAPI call', { body: nocontentAPI, contentType: 'image/png' });
   //Broken links - similar with links - will not be implemented
- */
+ 
 
   //Download file
   await page.locator(Locators.DownloadUpload.DownloadUploadMenuOption).click();
@@ -119,6 +119,8 @@ test('DemoQA Test Report', async ({ page }, testReport) => {
   const downloaduploadscreenshot = await page.screenshot();
   await testReport.attach('Upload - Download', { body: downloaduploadscreenshot, contentType: 'image/png' });
   await page.locator(Locators.Elements.ElementsMenuOption).click();
+ 
+
   //Forms
   await page.locator(Locators.Forms.FormsMenuOption).click();
   await page.locator(Locators.Forms.PracticeFormMenuOption).click();
@@ -132,7 +134,6 @@ test('DemoQA Test Report', async ({ page }, testReport) => {
   await page.locator(Locators.Forms.DateofBirth).fill(PracticeFormData.dob);
   await page.locator(Locators.Forms.SubjectContainer).click();
   await page.locator(Locators.Forms.SubjectContainer).fill(PracticeFormData.subject)
-  //await page.waitForTimeout(500);
   await page.getByText(PracticeFormData.hobby1).setChecked(true);
   await page.getByText(PracticeFormData.hobby2).setChecked(true);
   await page.getByText(PracticeFormData.hobby3).setChecked(true);
@@ -140,17 +141,58 @@ test('DemoQA Test Report', async ({ page }, testReport) => {
   await page.locator(Locators.Forms.UploadButton2).setInputFiles(Locators.Forms.UploadPath2);
   await page.mouse.wheel(0, 300);
   await page.locator(Locators.Forms.CurrentAddress).fill(PracticeFormData.currentaddress);
-
-
   await page.locator(Locators.General.Submit).click();
-  //await page.mouse.wheel(0, 300);
   const submitbutton2 = await page.screenshot();
-  await testReport.attach('Submit', { body: submitbutton2, contentType: 'image/png' });
+  await testReport.attach('Submit form', { body: submitbutton2, contentType: 'image/png' });
+  await page.waitForTimeout(300);
+  await page.locator(Locators.Forms.CloseLargeModal).click();
+ */
+  
+  //Alerts, Frames & Windows
+  await page.locator(Locators.AlertsWindowsFrames.AlertsWindowsFramesOption).click();
+  await page.locator(Locators.AlertsWindowsFrames.BrowserWindowsMenuOption).click();
+  const alertswindowsframes = await page.screenshot();
+  await testReport.attach('Alerts Windows Frames', { body: alertswindowsframes, contentType: 'image/png' });
+  await page.locator(Locators.AlertsWindowsFrames.NewTabButton).click();
+  await page.context().pages().at(1)?.bringToFront();
+  await page.waitForTimeout(1000);
+  const newtab = await page.context().pages().at(1)?.screenshot();
+  await testReport.attach('New Tab', { body: newtab, contentType: 'image/png' });
+  await page.context().pages().at(1)?.close();
+  await page.locator(Locators.AlertsWindowsFrames.NewWindowButton).click();
+  await page.context().pages().at(1)?.bringToFront();
+  await page.waitForTimeout(1000);
+  const newwindow = await page.context().pages().at(1)?.screenshot();
+  await testReport.attach('New Window', { body: newwindow, contentType: 'image/png' });
+  await page.context().pages().at(1)?.close();
+  await page.locator(Locators.AlertsWindowsFrames.MessageWindowButton).click();
+  await page.context().pages().at(1)?.bringToFront();
+  await page.waitForTimeout(1000);
+  const messagewindow = await page.context().pages().at(1)?.screenshot();
+  await testReport.attach('Message Window', { body: messagewindow, contentType: 'image/png' });
+  await page.context().pages().at(1)?.close();
+  await page.waitForTimeout(1000);
+  await page.locator(Locators.AlertsWindowsFrames.AlertsMenuOption).filter({hasText:'Alerts'}).click(); 
+  const alertsmenuoption = await page.screenshot();
+  await testReport.attach('Alerts menu', { body: alertsmenuoption, contentType: 'image/png' });
+  
+  /*
+  await page.waitForTimeout(2000);
+  page.on('dialog', dialog => dialog.accept());
+  const alertbutton1 = await page.screenshot();
+  await testReport.attach('Alerts button', { body: alertbutton1, contentType: 'image/png' });
+  await page.waitForTimeout(2000);
+  
+  await page.waitForTimeout(5000);
+  await page.mouse.wheel(0, 300);
+  page.on('dialog', dialog => dialog.dismiss());
+  await page.waitForTimeout(5000);
+  await page.locator(Locators.AlertsWindowsFrames.AlertButton).click();
+  await page.waitForTimeout(5000);
+  const alertbutton = await page.screenshot();
+  await testReport.attach('Alerts button', { body: alertbutton, contentType: 'image/png' });
+*/
 
-
-  await page.waitForTimeout(500);
-  const formspagefillin = await page.screenshot();
-  await testReport.attach('Forms menu', { body: formspagefillin, contentType: 'image/png' });
 
   //Logout and close
   await page.waitForTimeout(5000);
