874ac0b6eab4970141d6e1b5b25b24fb84dbf056
Adrian B <adryanb1980@yahoo.com>
2026-08-07 10:11:50 +0300

    Date personale 7 August 2026 A

--- tests/AppServ/ModulDatePersonale.spec.ts
+++ tests/AppServ/ModulDatePersonale.spec.ts
@@ -3,6 +3,10 @@ import * as Locators from '../../locators/AppServ/creareangajat.json';
 import * as LoginData from '../../test-data/AppServ/login.json';
 import * as GeneralLocators from '../../locators/AppServ/generalcontrols.json';
 import * as EmployeeData from '../../test-data/AppServ/creareangajat.json';
+import * as LocatorsDatePersonale from '../../locators/AppServ/datepersonalelocators.json'; 
+import * as DatePersonaleData from '../../test-data/AppServ/datepersonaledata.json';
+
+
 
 test('Creare puncte de lucru - in progress', async ({ page }) => {
   
@@ -20,7 +24,23 @@ test('Creare puncte de lucru - in progress', async ({ page }) => {
   //Click Puncte de lucru
   await page.getByText(Locators.MainMenu.PuncteDeLucru,{exact:true}).click();
   await page.locator(GeneralLocators.Butoane.Adaugare).click();
- 
+
+  //Nume
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.Nume);
+  //Tip
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tip).click();
+  await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.Tip}).click();
+  //Tara
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tara).click();
+  await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.Tara}).click();
+  //Judet
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Judet).click();
+  await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.Judet}).click();
+  //Localitate
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Localitate).click();
+  await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.Localitate}).click();
+
   //Logout
   //await page.waitForTimeout(4000);
   await page.locator(GeneralLocators.Logout.Logout).click();
