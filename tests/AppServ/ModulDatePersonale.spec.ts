1cb40e84b638ddbe7a722b828eb6315563b9344d
Adrian B <adryanb1980@yahoo.com>
2026-08-12 13:30:34 +0300

    Finalizat organizatii

--- tests/AppServ/ModulDatePersonale.spec.ts
+++ tests/AppServ/ModulDatePersonale.spec.ts
@@ -7,7 +7,7 @@ import * as LocatorsDatePersonale from '../../locators/AppServ/datepersonaleloca
 import * as DatePersonaleData from '../../test-data/AppServ/datepersonaledata.json';
 
 
-
+//Meniu mare - Puncte de lucru
 test('Creare puncte de lucru', async ({ page }) => {
   
   test.setTimeout(50000);
@@ -26,34 +26,38 @@ test('Creare puncte de lucru', async ({ page }) => {
   await page.locator(GeneralLocators.Butoane.Adaugare).click();
 
   //Nume
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Nume);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Nume);
   //Tip
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tip).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Tip).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Tip}).click();
   //Tara
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tara).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Tara).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Tara}).click();
   //Judet
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Judet).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Judet).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Judet}).click();
   //Localitate
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Localitate).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Localitate).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Localitate}).click();
   //Adresa
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Adresa).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Adresa).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Adresa);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Adresa).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Adresa).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Adresa);
   //CUI
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.CUI).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.CUI).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.CUI);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.CUI).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.CUI).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.CUI);
   //Salvare
   try {
     await page.locator(GeneralLocators.Butoane.Salveaza).click();
-    await page.waitForTimeout(2000);
   } catch (error) {
     console.error('Eroare la click pe butonul Salveaza:', error);   
   }
-  
+
+   //Afisare date punct de lucru creat
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.FiltruNume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.FiltruNume).clear();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.FiltruNume).fill(DatePersonaleData.PuncteDeLucru.CrearePuncteDeLucru.Nume);
+
   //Logout
   //await page.waitForTimeout(4000);
   await page.locator(GeneralLocators.Logout.Logout).click();
@@ -79,33 +83,33 @@ test('Editare puncte de lucru', async ({ page }) => {
   await page.getByText(Locators.MainMenu.PuncteDeLucru,{exact:true}).click();
   
   //Edit the component
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.NumeInitial);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.NumeInitial);
   await page.mouse.wheel(100, 0);
   await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.NumeInitial}).last().click({ position: { x: 30, y: 0 } }); 
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Modifica).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Modifica).click();
   
   //Nume
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Nume);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Nume);
   //Tip
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tip).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Tip).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Tip}).click();
   //Tara
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Tara).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Tara).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Tara}).click(); 
   //Judet
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Judet).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Judet).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Judet}).click();
   //Localitate
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Localitate).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Localitate).click();
   await page.getByRole('option', {name:DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Localitate,exact:true}).click();
   //Adresa
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Adresa).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.Adresa).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Adresa);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Adresa).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.Adresa).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Adresa);
   //CUI
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.CUI).click();
-  await page.locator(LocatorsDatePersonale.PuncteDeLucru.CUI).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.CUI);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.CUI).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.AdaugarePuncteDeLucru.CUI).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.CUI);
   //Salvare
   try {
     await page.locator(GeneralLocators.Butoane.Salveaza).click();
@@ -114,12 +118,12 @@ test('Editare puncte de lucru', async ({ page }) => {
   }
 
   //Afisare date punct de lucru editat
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Nume).clear();
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Nume);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Nume).clear();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Nume);
   await page.mouse.wheel(100, 0);
   await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.PuncteDeLucru.EditarePuncteDeLucru.Nume}).last().click({ position: { x: 30, y: 0 } }); 
-  await page.locator(LocatorsDatePersonale.EditarePuncteDeLucru.Modifica).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.EditarePuncteDeLucru.Modifica).click();
   await page.waitForTimeout(2000);
 
   //Logout
@@ -147,12 +151,12 @@ test('Stergere puncte de lucru', async ({ page }) => {
   await page.getByText(Locators.MainMenu.PuncteDeLucru,{exact:true}).click();
     
   //Edit the component
-  await page.locator(LocatorsDatePersonale.StergerePuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.StergerePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.StergerePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.StergerePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume);
   await page.mouse.wheel(100, 0);
   await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume}).last().click({ position: { x: 30, y: 0 } }); 
   
-  //Salvare
+  //Stergere
   try {
     //Sterge
     await page.locator(GeneralLocators.Butoane.Sterge).click();
@@ -163,23 +167,23 @@ test('Stergere puncte de lucru', async ({ page }) => {
   }
 
   //Afisare date punct de lucru sters
-  await page.locator(LocatorsDatePersonale.StergerePuncteDeLucru.Nume).click();
-  await page.locator(LocatorsDatePersonale.StergerePuncteDeLucru.Nume).clear();
-  await page.locator(LocatorsDatePersonale.StergerePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume);
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.StergerePuncteDeLucru.Nume).click();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.StergerePuncteDeLucru.Nume).clear();
+  await page.locator(LocatorsDatePersonale.PuncteDeLucru.StergerePuncteDeLucru.Nume).fill(DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume);
   await page.mouse.wheel(100, 0);
   await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.PuncteDeLucru.StergerePuncteDeLucru.Nume}); 
   await page.waitForTimeout(2000);
 
   //Logout
-  //await page.waitForTimeout(4000);
   await page.locator(GeneralLocators.Logout.Logout).click();
   await page.getByRole('menuitem', {name: GeneralLocators.Logout.Iesire, exact: true}).click();
   await page.context().close();
   await page.close();
 });
 
-/*
-test('Creare organizatii - in progress', async ({ page }) => {
+//Meniu mare - Organizatii
+
+test('Creare organizatii', async ({ page }) => {
   await page.goto(LoginData.link);
   
   //Acces meniu mare
@@ -188,21 +192,120 @@ test('Creare organizatii - in progress', async ({ page }) => {
   await page.locator(Locators.Login.Password).click();
   await page.locator(Locators.Login.Password).fill(LoginData.password);
   await page.locator(Locators.Login.Autentificare).click();
+  await page.locator(Locators.MainMenu.MainMenuAccess).click();
   
-  
-  
-  //await page.locator(Locators.MainMenu.MainMenuAccess).click();
-  
-  //Click angajati
-  //await page.getByText(Locators.MainMenu.Angajati,{exact:true}).click();
+  //Click Organizatii
+  await page.getByText(Locators.MainMenu.Organizatii,{exact:true}).click();
+  await page.locator(GeneralLocators.Butoane.Adaugare).click(); 
 
+  //Nume
+  await page.locator(LocatorsDatePersonale.Organizatii.AdaugareOrganizatii.Nume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.AdaugareOrganizatii.Nume).fill(DatePersonaleData.Organizatii.CreareOrganizatii.Nume);
+  //Implicita - trebuie sa fie una singura organizatie implicita, deci nu se poate seta alta organizatie implicita
+  //await page.locator(LocatorsDatePersonale.Organizatii.AdaugareOrganizatii.Implicita).click();
+
+  //Salvare
+  try {
+    await page.locator(GeneralLocators.Butoane.Salveaza).click();
+    await page.waitForTimeout(2000);
+  } catch (error) {
+    console.error('Eroare la click pe butonul Salveaza:', error);   
+  }
   
   //Logout
-  //await page.waitForTimeout(4000);
   await page.locator(GeneralLocators.Logout.Logout).click();
   await page.getByRole('menuitem', {name: GeneralLocators.Logout.Iesire, exact: true}).click();
-
   await page.context().close();
   await page.close();
 });
-*/
+
+
+test('Editare organizatii', async ({ page }) => {
+  await page.goto(LoginData.link);
+  
+  //Acces meniu mare
+  await page.locator(Locators.Login.UserName).click();
+  await page.locator(Locators.Login.UserName).fill(LoginData.username);
+  await page.locator(Locators.Login.Password).click();
+  await page.locator(Locators.Login.Password).fill(LoginData.password);
+  await page.locator(Locators.Login.Autentificare).click();
+  await page.locator(Locators.MainMenu.MainMenuAccess).click();
+  
+  //Click Organizatii
+  await page.getByText(Locators.MainMenu.Organizatii,{exact:true}).click();
+
+  //Edit the component
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.FiltrareNume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.FiltrareNume).fill(DatePersonaleData.Organizatii.EditareOrganizatii.NumeInitial);
+  await page.mouse.wheel(100, 0);
+  await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.Organizatii.EditareOrganizatii.NumeInitial}).last().click({ position: { x: 30, y: 0 } }); 
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.Modifica).click();
+
+  //Nume
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.Nume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.Nume).fill(DatePersonaleData.Organizatii.EditareOrganizatii.Nume);
+
+  //Salvare
+  try {
+    await page.locator(GeneralLocators.Butoane.Salveaza).click();
+  } catch (error) {
+    console.error('Eroare la click pe butonul Salveaza:', error);
+  }
+
+  //Afisare organizatie editata
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.FiltrareNume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.FiltrareNume).clear();
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.FiltrareNume).fill(DatePersonaleData.Organizatii.EditareOrganizatii.Nume);
+  await page.mouse.wheel(100, 0);
+  await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.Organizatii.EditareOrganizatii.Nume}).last().click({ position: { x: 30, y: 0 } }); 
+  await page.locator(LocatorsDatePersonale.Organizatii.EditareOrganizatii.Modifica).click();
+  await page.waitForTimeout(2000);
+
+  //Logout
+  await page.locator(GeneralLocators.Logout.Logout).click();
+  await page.getByRole('menuitem', {name: GeneralLocators.Logout.Iesire, exact: true}).click();
+  await page.context().close();
+  await page.close();
+});
+
+test('Stergere organizatii', async ({ page }) => {
+  await page.goto(LoginData.link);
+  
+  //Acces meniu mare
+  await page.locator(Locators.Login.UserName).click();
+  await page.locator(Locators.Login.UserName).fill(LoginData.username);
+  await page.locator(Locators.Login.Password).click();
+  await page.locator(Locators.Login.Password).fill(LoginData.password);
+  await page.locator(Locators.Login.Autentificare).click();
+  await page.locator(Locators.MainMenu.MainMenuAccess).click();
+  
+  //Click Organizatii
+  await page.getByText(Locators.MainMenu.Organizatii,{exact:true}).click();
+
+  //Edit the component
+  await page.locator(LocatorsDatePersonale.Organizatii.StergereOrganizatii.FiltrareNume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.StergereOrganizatii.FiltrareNume).fill(DatePersonaleData.Organizatii.StergereOrganizatii.Nume);
+  await page.mouse.wheel(100, 0);
+  await page.getByRole('gridcell').filter({ hasText: DatePersonaleData.Organizatii.StergereOrganizatii.Nume}).last().click({ position: { x: 30, y: 0 } }); 
+  
+  //Stergere
+  try {
+    //Sterge
+    await page.locator(GeneralLocators.Butoane.Sterge).click();
+    //Confirma
+    await page.locator(GeneralLocators.Butoane.Sterge).click();
+  } catch (error) {
+    console.error('Eroare la click pe butonul Sterge', error);
+  }
+
+  //Afisare organizatie stearsa
+  await page.locator(LocatorsDatePersonale.Organizatii.StergereOrganizatii.FiltrareNume).click();
+  await page.locator(LocatorsDatePersonale.Organizatii.StergereOrganizatii.FiltrareNume).clear();
+  await page.locator(LocatorsDatePersonale.Organizatii.StergereOrganizatii.FiltrareNume).fill(DatePersonaleData.Organizatii.StergereOrganizatii.Nume);
+  
+  //Logout
+  await page.locator(GeneralLocators.Logout.Logout).click();
+  await page.getByRole('menuitem', {name: GeneralLocators.Logout.Iesire, exact: true}).click();
+  await page.context().close();
+  await page.close();
+});
